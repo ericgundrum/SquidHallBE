4d8119c320d1d9924ee65de6f9a63933ba4ed368
Eric Gundrum <XPSe@ericgundrum.com>
2020-07-09 09:56:04 -0700

    Prepare for container deployment, including heroku.

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,12 +7,12 @@ import { Server } from "colyseus";
 import { GameRoom } from "./rooms/GameRoom";
 
 export const port = process.env.PORT || 2657;
+const static_dir = path.join(__dirname, "..", "..", "client", "dist");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "..", "client", "dist")));
+app.use(express.static(static_dir));
 
 // Create HTTP & WebSocket servers
 const server = http.createServer(app);
@@ -23,4 +23,5 @@ const gameServer = new Server({
 gameServer.define("SquidHall", GameRoom);
 
 server.listen(port);
-console.log(`Listening on ${ port }`)
+console.log(`Listening on ${ port }`);
+console.log("Serving static files from \'" + static_dir + "\'.");
