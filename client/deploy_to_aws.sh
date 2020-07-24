b7f2b5707895626d31706bf6ed594be71af6b98a
Eric Gundrum <XPSe@ericgundrum.com>
2020-07-24 13:40:42 -0700

    Fix `deploy_to_aws.sh` so that it actually works...
    Using this script to pull together disparate files
    and rename one or two feels hackish.
    Every call to `s3_cp` sends data whether or not the file is changed.
    We should find a more efficient solution.

--- client/deploy_to_aws.sh
+++ client/deploy_to_aws.sh
@@ -1,38 +1,36 @@
 #! /usr/bin/env sh
 # deploy to aws s3
 
-set -x
+set -e
 
-bkt={'s3://squidhall'}
+bkt='s3://squidhall'
+
+aws() {
+    sudo docker run -it --rm -v ~/.config/aws:/root/.aws -v $(pwd):/aws aws $@
+}
 
 s3_cp() {
     aws s3 cp $@ --storage-class REDUCED_REDUNDANCY
-    ;
 }
 
 s3_sync() {
     aws s3 sync $@ --storage-class REDUCED_REDUNDANCY
-    ;
 }
 
 for f in `ls squidhall/*.html` ; do
-    s3_cp squidhall/${f} ${bkt}/
-    ;
+    s3_cp ${f} ${bkt}/
 done
 
 for f in `ls squidhall/Gimble_*` ; do
-    s3_cp squidhall/${f} ${bkt}/
-    ;
+    s3_cp ${f} ${bkt}/
 done
 
-for d in (( audio/ ; css/ ; libs/ ; textures/ )) ; do
+for d in audio/ css/ libs/ textures/ ; do
     s3_sync squidhall/${d} ${bkt}/${d}
-    ;
 done
 
 for f in `ls dist/*.bundle.js*` ; do
-    s3_cp dist/${f} ${bkt}/
-    ;
+    s3_cp ${f} ${bkt}/
 done
 
 s3_cp dist/index.html ${bkt}/squidhalltest_mu.html
