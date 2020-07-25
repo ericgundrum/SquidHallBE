1bfc6a95c98ed3d3418c46c268196e9653b0f72d
Eric Gundrum <XPSe@ericgundrum.com>
2020-07-24 17:26:17 -0700

    Provide `make` script to gather all client assets and sync to aws s3.

---
+++ client/make.sh
@@ -0,0 +1,50 @@
+#! /usr/bin/env bash
+# make SquidHall clients
+#
+# `./make.sh` to populate 'dist/' with webpack bundles and squidhall assets
+# `./make.sh sync` to also sync 'dist/' to aws 's3://squidhall/'
+
+set -e
+
+aws() {
+    if [ -z `which aws` ] && [ `which docker` ] ; then
+        sudo docker run -it --rm -v ~/.config/aws:/root/.aws -v $(pwd):/aws aws $@
+    else
+        aws $@
+    fi
+}
+
+webpack() {
+    if [ -z `which npx` ] && [ `which docker` ] ; then
+        sudo docker run -it --rm -v `pwd`:/home/me node12 npx webpack $@
+    else
+        npx wepback $@
+    fi
+}
+
+# bundle squidhall babylon objects
+cd squidhall && ./buildall.sh && cd -
+
+# bundle webpack files into 'dist/'
+webpack --env.production
+
+# rename to allow for squidhall rooms
+mv dist/index.html dist/squidhall_mu.html
+
+# copy squidhall root files
+rsync -t squidhall/*.html dist/
+rsync -t squidhall/Gimble_* dist/
+
+# copy squidhall dirs
+for d in audio css libs textures ; do
+    rsync -tr squidhall/${d} dist/
+done
+
+# remove unused files
+rm dist/audio/crowdambiance.wav || true
+rm -rf dist/libs/modules/content/ || true
+
+# sync to aws with `make.sh sync`
+if [ ${1} ] && [ ${1} == 'sync' ] ; then
+    aws s3 sync dist/ s3://squidhall/ --delete --storage-class REDUCED_REDUNDANCY
+fi
