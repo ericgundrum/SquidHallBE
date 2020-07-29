178f62c36c525a41dd8ec58d23466029e03f43aa
Eric Gundrum <XPSe@ericgundrum.com>
2020-07-29 13:37:06 -0700

    Provide code to enable TLS using 'Lets Encrypt'...
    Easiest to test this on another machine.

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,25 +6,49 @@ import { Server } from "colyseus";
 
 import { GameRoom } from "./rooms/GameRoom";
 
-export const port = process.env.PORT || 2657;
-const static_dir = path.join(__dirname, "..", "..", "client", "dist");
+function setup(app: express.Application, server: http.Server) {
+    const gameServer = new Server({
+        pingInterval: 2000,
+        pingMaxRetries: 5,
+        server: server,
+        express: app
+    });
+    gameServer.define("SquidHall", GameRoom)
+        .filterBy(['title']);
 
+    return app;
+}
+
+const static_dir = path.join(__dirname, "..", "..", "client", "dist");
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static(static_dir));
 
-// Create HTTP & WebSocket servers
-const server = http.createServer(app);
-const gameServer = new Server({
-    pingInterval: 2000,
-    pingMaxRetries: 5,
-    server: server,
-    express: app
-});
-gameServer.define("SquidHall", GameRoom)
-          .filterBy(['title']);
+if (process.env.NODE_ENV === "production") {
+    require('greenlock-express')
+        .init({
+            packageRoot: __dirname,
+            configDir: "./greenlock.d",
+            maintainerEmail: 'me@example.com',
+            packageAgent: "squidhallmu" + "/" + "1.0",
+            cluster: false,
+    })
+    .ready(function (glx) {
+        // Serves on 80 and 443
+        // Get's SSL certificates magically!
+        glx.serveApp(setup(app, glx.httpsServer(undefined, app)));
+    });
+}
+else {
+    // development port
+    const PORT = process.env.PORT || 2657;
+    const server = http.createServer(app);
 
-server.listen(port);
-console.log(`Listening on ${ port }`);
-console.log("Serving static files from \'" + static_dir + "\'.");
+    setup(app, server);
+    server.listen(PORT, () =>
+        {
+            console.log(`Listening on http://localhost:${PORT}`);
+            console.log("Serving static files from \'" + static_dir + "\'.");
+        });
+}
